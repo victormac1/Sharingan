c7300dfeccf657c9f624e660d8b3cb564f3ac6de
Victor Mac <93493810+victormac1@users.noreply.github.com>
2022-03-28 17:11:56 +0000

    Add files via upload

---
+++ SharinganSwiftUI/SharinganSwiftUI/SharinganSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharinganSwiftUIApp.swift
+//  SharinganSwiftUI
+//
+//  Created by Victor Mac on 14/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SharinganSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
